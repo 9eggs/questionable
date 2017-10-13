7ef15185fa7dc839438100bf8d4dd1ea9c521d1f
Mike Morley <mike.morley@9eggs.com>
2017-10-13 07:11:44 +0100

    Input component

--- src/components/Input.test.tsx
+++ src/components/Input.test.tsx
@@ -5,7 +5,12 @@ import Input from './Input';
 import * as Adapter from 'enzyme-adapter-react-16';
 enzyme.configure({ adapter: new Adapter() });
 
-it('renders the correct input', () => {
-    const input = enzyme.shallow(<Input type="email"/>);
+it('renders the correct input type', () => {
+    const input = enzyme.shallow(<Input type="email" id="x" name ="x" value=""/>);
     expect(input.html()).toContain('type="email"');
+    expect(input.html()).toContain('id="x"');
+    expect(input.html()).toContain('name="x"');
+    expect(input.html()).toContain('value=""');
+    expect(input.html()).toContain('required');
+    expect(input.html()).toContain('aria-required="true"');
 });
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,15 +2,14 @@ import * as React from 'react';
 import './Input.css';
 export interface Props {
     type: string;
-    // id: string;
-    // name: string;
-    // value: string;
+    id: string;
+    name: string;
+    value: string;
 }
 
-function Input({type}: Props) {
+function Input({type, id, name, value}: Props) {
     return (
-        //<input type={type} id={id} name={name} value={value} aria-required="true" required/>
-        <input type={type}/>
+        <input type={type}  id={id} name={name} value={value} aria-required="true" required/>
     );
 }
 
